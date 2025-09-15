7e3b8ade22a9f7108dc97d05ba03131a1b06e195
Nour El Islam SAIDI <nour.saidi@blablacar.com>
2025-09-15 14:58:26 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "PlayerKMP",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(name: "PlayerKMP", targets: ["PlayerKMP"])
     ],
