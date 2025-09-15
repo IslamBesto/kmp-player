bfedca80a80e3667db28f4087d926c76e84e447b
Nour El Islam SAIDI <nour.saidi@blablacar.com>
2025-09-15 15:58:13 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@ import PackageDescription
 
 let package = Package(
     name: "PlayerKMP",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "PlayerKMP", targets: ["PlayerKMP"])
     ],
     targets: [
         .binaryTarget(
             name: "PlayerKMP",
-            url: "https://nexus-production.dnm.radiofrance.fr/repository/test-mobile-cop-spm/PlayerKMP/1.73-dev5-test15/PlayerKMP.xcframework.zip",
-            checksum: "19196ef92403201cb9ba3a3dfab7af3fe175065a1d125b5321d6199615a7857e"
+            url: "https://nexus-production.dnm.radiofrance.fr/repository/test-mobile-cop-spm/PlayerKMP/1.73-dev5-test14/PlayerKMP.xcframework.zip",
+            checksum: "fa00a9d6887028585e75e3ad546526fe65d42e9a4e4fcb5e26bfd8e544b2ef4d"
         )
     ]
 )
